8febb40a12bb3180a3c39935fba542779a5d7315
Nabila-0 <nabila.t0330@.com>
2026-03-16 14:22:27 +0600

    displayed all cards in all container

--- script/home.js
+++ script/home.js
@@ -14,8 +14,8 @@ const closedContainer = document.getElementById("closed_container");
 
 // function for Switching three Buttons 
 
-function switchTab (tab) {
-    
+function switchTab(tab) {
+
     const tabs = ["all", "open", "closed"];
 
     currentTab = tab;
@@ -62,14 +62,157 @@ function switchTab (tab) {
         closedContainer.classList.remove("hidden");
     }
 
-    
+    // updateStat();
+
 }
 
 switchTab(currentTab);
 
 
+// --------------
+
 // getting  Card Count by ID 
 
 const cardCount = document.getElementById("card_count");
 
+
+//----------------------------------
+
+
+const loadAllIssues = () => {
+    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+        .then((res) => res.json())
+        .then((json) => displayAllIssues(json.data));
+};
+
+const displayAllIssues = (allIssues) => {
+    // console.log(allIssues);
+
+    const allContainerDiv = document.getElementById("all_container_div");
+
+    allContainerDiv.innerHTML = "";
+
+    allIssues.forEach(issue => {
+        console.log(issue);
+
+        const issueCard = document.createElement("div");
+
+        issueCard.innerHTML = `
+
+            <div class="issue_card shadow-md p-4 border-t-4 rounded-md h-full
+            ${issue.status === "open"
+                ? 'border-[#00A96E]'
+                : 'border-[#A855F7]'
+            }">
+
+                <div class="upper flex justify-between">
+
+                    <div class="upper_left">
+
+
+                        ${issue.status === "open"
+
+                ? `<div class="bg-[#CBFADB] w-6 h-6 rounded-full    flex justify-center items-center">
+
+                    <div class="border-2 border-[#00A96E] border-dashed rounded-full w-4 h-4">
+                    </div>
+                </div>`
+
+                : `<div class="bg-[#F0E2FF] w-6 h-6 rounded-full relative">
+
+                    <div class="text-[#A855F7] rounded-full w-4 h-4 
+                    absolute left-0.5">
+                        <i class="fa-regular fa-circle-check"></i>
+                    </div>
+                </div>`
+            }
+
+                </div>
+
+                <div class="upper_right">
+                    <span class="text-[#EF4444] text-[12px] font-medium bg-[#FEECEC] py-1.5 px-[25.5px] rounded-[100px]">
+                    ${issue.priority.toUpperCase()}
+                    </span>
+                                </div>
+                            </div>
+
+
+                            
+                <div class="middle">
+
+                    <h2 class="text-[#1F2937] text-[14px] font-semibold mt-3 mb-2">
+                        ${issue.title}</h2>
+
+                    <p class="text-[#64748B] text-[12px] font-normal">
+                        ${issue.description}</p>
+
+                    <!-- two lebels div  -->
+
+                    <div class="two_lebels flex gap-1 mt-3 mb-4">
+
+                        <!-- Bug lebel  -->
+
+                        <div class="text-[#EF4444] text-[12px] font-medium 
+                        bg-[#FEECEC] rounded-[100px] border border-[#FECACA] px-2 
+                        py-1.5 flex gap-0.5">
+
+                            <span><i class="fa-solid fa-bug"></i></span>
+
+                            <span>BUG</span>
+                         </div>
+
+                        <!-- Help wanted lebel  -->
+
+                        <div class="text-[#D97706] text-[12px] font-medium 
+                        bg-[#FFF8DB] rounded-[100px] border border-[#FDE68A] px-2 
+                        py-1.5 flex gap-0.5">
+
+                            <span><i class="fa-solid fa-life-ring"></i></span>
+
+                                <span>HELP WANTED</span>
+                                    </div>
+
+                                </div>
+                            </div>
+
+
+
+                <div class="lower">
+
+                    <hr class="border border-[#E4E4E7] -mx-4">
+
+                    <div class="author_and_createdAt 
+                    text-[#64748B] text-[12px] font-normal flex justify-between mt-4 mb-2">
+
+                        <p>
+                            #${issue.id} by ${issue.author}</p>
+
+                        <p>${new Date(issue.createdAt).toLocaleDateString('en-US')}</p>
+
+                    </div>
+
+                    <div class="assignee_and_updatedAt 
+                    text-[#64748B] text-[12px] font-normal flex justify-between">
+
+                        <p>
+                            Assignee:${issue.assignee}
+                        </p>
+
+                        <p>Updated:${new Date(issue.updatedAt).toLocaleDateString('en-US')}</p>
+
+                    </div>
+
+                            </div>
+
+                        </div>
+            `;
+
+
+        allContainerDiv.append(issueCard);
+    });
+
+
+};
+
+loadAllIssues();
 
